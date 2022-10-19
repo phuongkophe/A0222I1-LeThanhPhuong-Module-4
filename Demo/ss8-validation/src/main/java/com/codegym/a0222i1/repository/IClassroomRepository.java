de988bd867a033fc1906dc630fb37ad69cf3d2ba
phuongkophe <phuong230602@gmail.com>
2022-10-19 16:13:16 +0700

    up

--- Demo/ss8-validation/src/main/java/com/codegym/a0222i1/repository/IClassroomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.a0222i1.repository;
-
-import com.codegym.a0222i1.model.ClassRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IClassroomRepository extends JpaRepository<ClassRoom, Integer> {
-}
--- Demo/ss8-validation/src/main/java/com/codegym/a0222i1/service/IClassroomService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.a0222i1.service;
-
-import com.codegym.a0222i1.model.ClassRoom;
-
-import java.util.List;
-
-public interface IClassroomService {
-    List<ClassRoom> findAll();
-}
